03c1169c8d4f54a31a222ee4bba026edd1a2be0d
ZakariyeAden <zakariye5417@gmail.com>
2023-06-12 19:50:32 -0500

    have a bug it only selects scepter

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ $(document).ready(onReady);
 // Example:
 let fungusHP = 100;
 let attackPoints = 100;
-
+let scepter;
+let entangle;
+let dragon;
+let star;
 function onReady() {
   // Make sure you check the index.html file!
   // There are lots of buttons and things ready for you to hook into here!
-  $(".arcane-scepter").on("click", attackEnemy);
-  $(".entangle").on("click", attackEnemy);
-  $(".dragon-blade").on("click", attackEnemy);
-  $(".star-fire").on("click", attackEnemy);
+  scepter = $(".arcane-scepter").on("click", attackEnemy);
+  entangle = $(".entangle").on("click", attackEnemy);
+  dragon =$(".dragon-blade").on("click", attackEnemy);
+  star = $(".star-fire").on("click", attackEnemy);
 
   // 🧠 Remember
   // - Handle events that ->
@@ -23,35 +26,40 @@ function onReady() {
 }
 
 function attackEnemy() {
-  if ($(".arcane-scepter")) {
+  if(scepter) {
     // Health points
     fungusHP -= 14;
     // Attack points
     attackPoints -= 12;
-    console.log("attack", fungusHP);
+    console.log("HP in scepter", fungusHP);
+    console.log("AP in scepter", attackPoints);
     render();
-  } else if ($(".entangle")) {
+  }  else if(entangle) {
     // Health points
     fungusHP -= 9;
     // Attack points
     attackPoints -= 23;
-    console.log("attack", fungusHP);
+    console.log("HP in entangle", fungusHP);
+    console.log("AP in entangle", attackPoints);
     render();
-  } else if ($(".dragon-blade")) {
+  }  else if(dragon) {
     // Health points
     fungusHP -= 47;
     // Attack points
     attackPoints -= 38;
-    console.log("attack", fungusHP);
+    console.log("HP in dragon", fungusHP);
+    console.log("AP in dragon", attackPoints);
     render();
-  } else if ($(".star-fire")) {
+  }  else if(star) {
     // Health points
     fungusHP -= 33;
     // Attack points
-    attackPoints -= 38;
-    console.log("attack", fungusHP);
+    attackPoints -= 100;
+    console.log("HP in star", fungusHP);
+    console.log("AP in star", attackPoints);
     render();
   }
+  
 }
 function render() {
   // Update the texts
@@ -62,5 +70,6 @@ function render() {
     $(".walk").addClass("dead");
   } else if (attackPoints < 0) {
     $(".walk").addClass("jump");
+    $(".attack-btn").addClass("disabled");
   }
 }
