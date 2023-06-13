26f8c9ccdb23d0766ee7df12590f7b4176c01cc9
ZakariyeAden <zakariye5417@gmail.com>
2023-06-12 19:21:46 -0500

    adding the attack points and FungusHP

--- script.js
+++ script.js
@@ -7,12 +7,10 @@ $(document).ready(onReady);
 let fungusHP = 100;
 let attackPoints = 100;
 
-
 // ### ![arcane scepter](./images/arcane-scepter.png) Arcane Scepter
 
 // - **AP Cost:** 12
 // - **HP Damage:** 14
-
 
 // ### ![entangle](./images/entangle-3.png) Entangle
 
@@ -29,43 +27,53 @@ let attackPoints = 100;
 // - **AP Cost:** 33
 // - **HP Damage:** 25
 function onReady() {
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    $('.arcane-scepter').on('click', attackEnemy);
-    $('.entangle').on('click', attackEnemy);
-    $('.dragon-blade').on('click', attackEnemy);
-    $('.star-fire').on('click', attackEnemy);
-    
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
-    render()
+  // Make sure you check the index.html file!
+  // There are lots of buttons and things ready for you to hook into here!
+  $(".arcane-scepter").on("click", attackEnemy);
+  $(".entangle").on("click", attackEnemy);
+  $(".dragon-blade").on("click", attackEnemy);
+  $(".star-fire").on("click", attackEnemy);
+
+  // 🧠 Remember
+  // - Handle events that ->
+  // - Updates state which is ->
+  // - Rendered to the DOM
+  render();
 }
 
-function attackEnemy(){
-    if($('.arcane-scepter')){
-        fungusHP -= 14;
-        console.log('attack', fungusHP);
-        render();
-    }else if($('.entangle')){
-        fungusHP -= 23;
-        console.log('attack', fungusHP);
-        render();
-    }else if($('.dragon-blade')){
-        fungusHP -= 38;
-        console.log('attack', fungusHP);
-        render();
-    }else if($('.star-fire')){
-        fungusHP -= 33;
-        console.log('attack', fungusHP);
-        render();
-    }else if( fungusHP === 0){
-        console.log('Stop');
-    }
-    
+function attackEnemy() {
+  if ($(".arcane-scepter")) {
+    // Health
+    fungusHP -= 14;
+    // Attack
+    attackPoints -= 12;
+    console.log("attack", fungusHP);
+    render();
+  } else if ($(".entangle")) {
+    // Health
+    fungusHP -= 9;
+    // Attack
+    attackPoints -= 23;
+    console.log("attack", fungusHP);
+    render();
+  } else if ($(".dragon-blade")) {
+    // Health
+    fungusHP -= 47;
+    // Attack
+    attackPoints -= 38;
+    console.log("attack", fungusHP);
+    render();
+  } else if ($(".star-fire")) {
+    // Health
+    fungusHP -= 33;
+    // Attack
+    attackPoints -= 38;
+    console.log("attack", fungusHP);
+    render();
+  }
 }
-function render(){
-   
-   let text = $('.hp-text').text(`${fungusHP} HP`)
+function render() {
+  // Update the texts
+  $(".hp-text").text(`${fungusHP} HP`);
+  $(".ap-text").text(`${attackPoints} AP`);
 }
